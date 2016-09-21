547f1d558785da460bb58e87ec3992c575a0b687
Nazar Holyanskyy <golyanskiy@gmail.com>
2016-09-21 12:20:35 -0400

    no message

--- quiz1school/studentview.php
+++ quiz1school/studentview.php
@@ -1,49 +1,25 @@
 <?php
 
 
-function getForm($iii) {
-    $f = <<< ENDTAG
-        <p>Are you sure you want to see student record number $iii ?</p>
-        
-        <form action="index.php">
-            <input type="submit" value="Cancel">
-        </form>
-        
-        <form>
-            <input type="hidden" name="id" value="$iii">
-            <input type="hidden" name="confirmed" value="yes">
-            <input type="submit" value="View">
-        </form>
-ENDTAG;
-    return $f;
-}
-
-if (isset($_GET['confirmed'])) {
     $id = $_GET['id'];
     
     require_once 'db.php';
     
-       
-    
-    $sql = sprintf("SELECT FROM student WHERE ID='%d'", mysqli_escape_string($conn, $id));
+        
+    $sql = sprintf("SELECT * FROM student WHERE ID='%d'", mysqli_escape_string($conn, $id));
     
     $result = mysqli_query($conn, $sql);
     if (!$result) {
         die("Error executing query [ $sql ] : " . mysqli_error($conn));
     }
     
-    while ($row = mysql_fetch_assoc($result)) {
-    echo $row["id"];
+    $row = mysqli_fetch_assoc($result);
+            
+    echo $row["ID"];
     echo $row["name"];
     echo $row["age"];
     echo $row["gpa"];
-    echo $row["graduated"];
-}
+    echo $row["hasGraduated"];
 
-mysql_free_result($result);
-    
+   
     echo "<a href=\"index.php\">Click to continue</a>\n";
-} else {
-    
-    echo getForm($_GET['id']);
-}
